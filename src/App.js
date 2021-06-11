3790638570d4d6903132d36b3ae8adbedd177fc8
Victor <victorv24@gmail.com>
2021-06-10 18:47:55 -0700

    Functional component is using React hooks and fetching SWAPI to console.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+import React, { useState, useEffect } from 'react';
+// import { Table, Row, Col } from 'react-bootstrap';
+
 function App () {
+  // const []
+
+  useEffect( () => {
+    fetch( "https://swapi.dev/api/people/1" ).then( response => response.json() ).then( data => console.log( 'data', data ) )
+  } )
+
   return (
     <div>
       <h1>SWAPI</h1>
