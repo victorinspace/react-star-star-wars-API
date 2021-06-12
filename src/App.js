da05265caf0346c22d98cbaa02d5d650e219d59a
Victor <victorv24@gmail.com>
2021-06-11 20:10:08 -0700

    Installs React Bootstrap.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from 'react';
 // import { Table, Row, Col } from 'react-bootstrap';
 
 function App () {
-  // const []
+  const [characterName] = useState
 
   useEffect( () => {
-    fetch( "https://swapi.dev/api/people/1" ).then( response => response.json() ).then( data => console.log( 'data', data ) )
+    fetch( "https://swapi.dev/api/people/1" )
+      .then( response => response.json() )
+      .then( data => console.log( 'data', data ) )
   } )
 
   return (
     <div>
       <h1>SWAPI</h1>
+      <h3>{characterName}</h3>
     </div>
   );
 }
