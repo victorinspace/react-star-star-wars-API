19c36e68a581f13979feefcee957a487db92a196
Victor <victorv24@gmail.com>
2021-06-13 13:47:16 -0700

    Adds variables for other fields / Begins using Axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Container, Table, Row, Col } from 'react-bootstrap';
 
 const App = () => {
-  const [name, setState] = useState( "" )
+  const [name, setName] = useState( 'https://swapi.dev/api/people/1' );
+  const [birthDate, setBirthDate] = useState( 'https://swapi.dev/api/people/1' );
+  const [height, setHeight] = useState( 'https://swapi.dev/api/people/1' );
+  const [mass, setMass] = useState( 'https://swapi.dev/api/people/1' );
+  const [homeworld, setHomeworld] = useState( 'https://swapi.dev/api/people/1' );
+  const [species, setSpecies] = useState( 'https://swapi.dev/api/people/1' );
 
-  useEffect( () => {
-    fetch( "https://swapi.dev/api/people/1" )
-      .then( response => response.json() )
-      .then( data => {
-        setState( data.name )
-      } )
-  } )
+  // const fetchData = () => {
+  //   const characterAPI = 'https://swapi.dev/api/people/1';
+
+  //   const getCharacterName = axios.get(characterAPI);
+  // }
+
+
+  // const name = 'https://swapi.dev/api/people/1';
+  // const birthDate = 'https://swapi.dev/api/people/';
+
+  const requestName = axios.get( name.name );
+  const requestBirthDate = axios.get( birthDate.birth_year );
+  const requestHeight = axios.get( height.height );
+  const requestMass = axios.get( mass.mass );
+  const requestHomeworld = axios.get( homeworld.homeworld );
+  const requestSpecies = axios.get( species.species );
+
+
+  axios.all( [requestName, requestBirthDate, requestHeight, requestMass, requestHomeworld, requestSpecies] ).then( axios.spread( ( ...responses ) => {
+    const nameResponse = responses[0];
+    const birthDateResponse = responses[1];
+    const heightResponse = responses[2];
+    const massResponse = responses[3];
+    const homeworldResponse = responses[4];
+    const speciesResponse = responses[5];
+  } ) ).catch( errors => console.error( 'error' ) );
+
+
+  // requestName.then(response => console.log(response) ).catch(error => console.error(error))
+
+
+  // useEffect( () => {
+  //   fetch( `${peopleAPI}1` )
+  //     .then( response => response.json() )
+  //     .then( data => {
+  //       setName( data.name );
+  //     } );
+  // }, [setName] );
 
   return (
     <Container>
@@ -41,11 +78,11 @@ const App = () => {
             <tbody>
               <tr>
                 <td>{name}</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>{birthDate}</td>
+                <td>{height}</td>
+                <td>{mass}</td>
+                <td>{homeworld}</td>
+                <td>{species}</td>
               </tr>
             </tbody>
           </Table>
@@ -53,6 +90,6 @@ const App = () => {
       </Row>
     </Container>
   );
-}
+};
 
 export default App;
