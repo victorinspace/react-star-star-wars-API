41f6625756c9e34b6aa61b6e968e18f86b36f189
Victor <victorv24@gmail.com>
2021-06-19 14:41:40 -0700

    Creates Search component / Adds event handlers inside Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 const Search = ( props ) => {
+  const [searchValue, setSearchValue] = useState( '' );
+
+  const handleSearchChanges = ( e ) => {
+    setSearchValue( e.target.value );
+  };
+
+  const searchCharacterData = ( e ) => {
+    e.preventDefault();
+    props.search( searchValue );
+  };
+
   return (
     <Form>
       <Form.Group>
-        <Form.Control type='search' placeholder='character search' />
+        <Form.Control
+          type='search'
+          placeholder='character search'
+          value={searchValue}
+          onChange={handleSearchChanges}
+        />
       </Form.Group>
-      <Button variant='warning' type='submit'>Find</Button>
+      <Button
+        onClick={searchCharacterData}
+        variant='warning'
+        type='submit'
+      >
+        Find
+      </Button>
     </Form>
   );
 };
